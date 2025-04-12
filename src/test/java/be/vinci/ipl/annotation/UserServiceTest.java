90b3b5330612361e5c632fa146fc5ef6105a848f
Robin Lamotte <163841689+robin-lamotte-vinci@users.noreply.github.com>
2025-04-12 16:40:06 +0200

    test: add unit tests for user registration in UserService

--- src/test/java/be/vinci/ipl/annotation/UserServiceTest.java
+++ src/test/java/be/vinci/ipl/annotation/UserServiceTest.java
@@ -2,14 +2,19 @@ package be.vinci.ipl.annotation;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import be.vinci.ipl.annotation.models.dtos.NewUser;
 import be.vinci.ipl.annotation.models.entities.User;
 import be.vinci.ipl.annotation.models.entities.User.Types;
 import be.vinci.ipl.annotation.repositories.UserRepository;
 import be.vinci.ipl.annotation.services.UserService;
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -25,6 +30,28 @@ class UserServiceTest {
 	@InjectMocks
 	private UserService userService;
 
+	private NewUser newUser;
+	private User user;
+
+	@BeforeEach
+	void setUp() {
+		newUser = new NewUser();
+		newUser.setUsername("Erwin");
+		newUser.setPassword("Schrodinger'sCat");
+		newUser.setCreatorUsername("Isaac");
+		newUser.setType(Types.USER);
+
+		User creator = new User(); // should be completed with coherent data
+		creator.setUsername(newUser.getCreatorUsername());
+		creator.setType(Types.ADMIN);
+
+
+		user = new User();
+		user.setUsername(newUser.getUsername());
+		user.setPassword(newUser.getPassword());
+		user.setCreator(creator);
+		user.setType(newUser.getType());
+	}
 
 	@Test
 	void testGetAllUsers() {
@@ -49,7 +76,61 @@ class UserServiceTest {
 				"The returned user list does not match the expected list.");
 	}
 
+	@Test
+	void testRegisterWithValidData() {
+		// Arrange
+		when(userRepository.existsByUsername(newUser.getUsername())).thenReturn(false);
+		when(userRepository.findByUsername(newUser.getCreatorUsername())).thenReturn(user.getCreator());
+		when(userRepository.save(any(User.class))).thenReturn(user);
 
+		// Act
+		User result = userService.register(newUser);
+
+		// Assert
+		assertNotNull(result,
+				"Register a user with valid data should return an User instance.");
+	}
+
+	@Test
+	void testRegisterWithExistingUsername() {
+		// Arrange
+		when(userRepository.existsByUsername(newUser.getUsername())).thenReturn(true);
+
+		// Act
+		User result = userService.register(newUser);
+
+		// Assert
+		assertNull(result,
+				"Register a user with existing username should return null.");
+	}
+
+	@Test
+	void testRegisterWithUnknownCreatorUser() {
+		// Arrange
+		when(userRepository.existsByUsername(newUser.getUsername())).thenReturn(false);
+		when(userRepository.findByUsername(newUser.getCreatorUsername())).thenReturn(null);
+
+		// Act
+		User result = userService.register(newUser);
+
+		// Assert
+		assertNull(result,
+				"Register a user with unknown creator user should return null.");
+	}
+
+	@Test
+	void testRegisterWithNonAdminCreator() {
+		// Arrange
+		when(userRepository.existsByUsername(newUser.getUsername())).thenReturn(false);
+		when(userRepository.findByUsername(newUser.getCreatorUsername())).thenReturn(user);
+
+		// Act
+		User result = userService.register(newUser);
+
+		// Assert
+		assertNull(result,
+				"Register a user with non admin creator user should return null.");
+	}
 
 
 }
