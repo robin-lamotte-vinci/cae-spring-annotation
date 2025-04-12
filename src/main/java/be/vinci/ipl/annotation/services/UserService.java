e2315a3cd3f45b951a1ba4cb41e0b59b889464b1
Robin Lamotte <163841689+robin-lamotte-vinci@users.noreply.github.com>
2025-04-12 16:39:59 +0200

    feat: add register method to UserService for user registration

--- src/main/java/be/vinci/ipl/annotation/services/UserService.java
+++ src/main/java/be/vinci/ipl/annotation/services/UserService.java
@@ -1,7 +1,9 @@
 package be.vinci.ipl.annotation.services;
 
 
+import be.vinci.ipl.annotation.models.dtos.NewUser;
 import be.vinci.ipl.annotation.models.entities.User;
+import be.vinci.ipl.annotation.models.entities.User.Types;
 import be.vinci.ipl.annotation.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -29,5 +31,33 @@ public class UserService {
    */
   public Iterable<User> getAllUsers() {
     return userRepository.findAll();
+  }
+
+  /**
+   * Register a new user.
+   *
+   * @param newUser the new user to register (assuming he is valid)
+   * @return registered User when the registration is successful, null otherwise
+   */
+  public User register(NewUser newUser) {
+    String username = newUser.getUsername();
+    // registration fail if username already exists
+    if (userRepository.existsByUsername(username)){
+      return null;
+    }
+
+    User creator = userRepository.findByUsername(newUser.getCreatorUsername());
+    // registration should if creatorUsername isn't the username of an existing user of type admin
+    if (creator == null || creator.getType() != Types.ADMIN){
+      return null;
+    }
+
+    User user = new User();
+    user.setUsername(newUser.getUsername());
+    user.setPassword(newUser.getPassword());
+    user.setCreator(creator);
+    user.setType(newUser.getType());
+
+    return userRepository.save(user);
   }
 }
