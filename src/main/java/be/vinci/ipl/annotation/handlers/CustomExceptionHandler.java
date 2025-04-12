c5fa424b403cb92e2fed49c3a7300793c89f6f7e
Robin Lamotte <163841689+robin-lamotte-vinci@users.noreply.github.com>
2025-04-12 17:09:06 +0200

    feat: add CustomExceptionHandler for handling UniqueUsernameException and returning a conflict HTTP status

---
+++ src/main/java/be/vinci/ipl/annotation/handlers/CustomExceptionHandler.java
@@ -0,0 +1,26 @@
+package be.vinci.ipl.annotation.handlers;
+
+import be.vinci.ipl.annotation.exceptions.UniqueUsernameException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * A custom exception handler to manage application-specific exceptions.
+ */
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+  /**
+   * Handles the UniqueUsernameException and returns a conflict HTTP status.
+   *
+   * @param ex the UniqueUsernameException thrown
+   * @return a ResponseEntity containing the exception message and HTTP status
+   */
+  @ExceptionHandler(UniqueUsernameException.class)
+  public ResponseEntity<String> handleUniqueEmailException(UniqueUsernameException ex) {
+    return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+  }
+
+}
