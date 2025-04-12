b2c87e64a52a36247bcdd10a588f67db9a5addff
Robin Lamotte <163841689+robin-lamotte-vinci@users.noreply.github.com>
2025-04-12 16:10:00 +0200

    feat: add NewUser DTO for user creation

---
+++ src/main/java/be/vinci/ipl/annotation/models/dtos/NewUser.java
@@ -0,0 +1,26 @@
+package be.vinci.ipl.annotation.models.dtos;
+
+import be.vinci.ipl.annotation.models.entities.User.Types;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * NewUser DTO.
+ */
+@Data
+@NoArgsConstructor
+public class NewUser {
+  @NotBlank
+  private String username;
+
+  @NotBlank
+  private String password;
+
+  @NotBlank
+  private String creatorUsername;
+
+  @NotNull
+  private Types type;
+}
