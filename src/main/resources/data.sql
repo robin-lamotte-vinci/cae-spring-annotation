4ca07dcce2d185004e12741b144a2a03e561eacc
Robin Lamotte <163841689+robin-lamotte-vinci@users.noreply.github.com>
2025-04-12 16:02:11 +0200

    feat: add initial user data to populate the users table

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (username, password, creator, type)
+VALUES ('Isaac', 'kamoulox', NULL, 'ADMIN'),
+       ('Galileo', 'mustardsupplement', 1, 'USER'),
+       ('Léonard', 'engineerskills', 1, 'USER');
