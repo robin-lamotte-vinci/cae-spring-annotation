bde2a1ece1df8237a773e7a45abcdd606f3b6fc7
Robin Lamotte <163841689+robin-lamotte-vinci@users.noreply.github.com>
2025-04-13 02:42:54 +0200

    feat: add @ResponseStatus(HttpStatus.CONFLIT) annotation to UniqueUsernameException to show that's possible to handle custom exceptions with powerful annotations and without global handler -- hasn't been tested, the annotation 'll stay commented

--- src/main/java/be/vinci/ipl/annotation/exceptions/UniqueUsernameException.java
+++ src/main/java/be/vinci/ipl/annotation/exceptions/UniqueUsernameException.java
@@ -1,8 +1,12 @@
 package be.vinci.ipl.annotation.exceptions;
 
+// import org.springframework.http.HttpStatus;
+// import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Exception thrown when a unique username constraint is violated.
  */
+// @ResponseStatus(HttpStatus.CONFLICT)
 public class UniqueUsernameException extends RuntimeException {
 
   /**
